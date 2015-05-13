e3c771ae7e4daa7bcc69cd9c4788bfecdf78fc74
Admi <Admi@Admi-PC.localdomain>
2015-05-13 18:12:26 +0200

    f

---
+++ loginView.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+      <?php require_once 'layout/header.php' ?>
+  </head>
+  <body>
+    
+      
+    <?php 
+        require_once 'views/login.php';
+        require_once 'layout/footer.php';    
+      ?>
+   
+  </body>
+</html>
