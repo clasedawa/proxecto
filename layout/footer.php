d2d0f9ebe96f0f51fc0c9d5daf024ee7486e294b
Enrique Prado <mansun@hotmail.es>
2015-05-20 19:37:38 +0200

    Arreglado el Head

--- layout/footer.php
+++ layout/footer.php
@@ -2,3 +2,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
---
+++ layout/menu.php
@@ -0,0 +1,60 @@
+<div id="sidebar-wrapper">
+            <h2>Menú</h2>
+            <ul class="sidebar-nav">
+                <!-- tienen que llevar la clase .fa-caret-up siempre que haya desplegable-->
+                <li>
+                    <a href="#"><i class="flaticon-calendar135"></i> Cursos<i class="fa fa-caret-up"></i></a>
+                    <ul>
+                        <li><a href="curso-view.php"><i class="fa fa-eye"></i> Consulta</a></li>
+                        <li><a href="curso-edit.php"><i class="fa fa-pencil"></i> Modificación</a></li>
+                        <li><a href="curso-add.php"><i class="fa fa-plus-circle"></i> Alta</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="flaticon-graduate20"></i> Ciclos<i class="fa fa-caret-up"></i></a>
+                    <ul>
+                        <li><a href="ciclo-view.php"><i class="fa fa-eye"></i> Consulta</a></li>
+                        <li><a href="ciclo-edit.php"><i class="fa fa-pencil"></i> Modificación</a></li>
+                        <li><a href="ciclo-add.php"><i class="fa fa-plus-circle"></i> Alta</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="flaticon-notepad10"></i> Evaluaciones<i class="fa fa-caret-up"></i></a>
+                    <ul>
+                        <li><a href="evaluacion-view.php"><i class="fa fa-eye"></i> Consulta</a></li>
+                        <li><a href="evaluacion-edit.php"><i class="fa fa-pencil"></i> Modificación</a></li>
+                        <li><a href="evaluacion-add.php"><i class="fa fa-plus-circle"></i> Alta</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="flaticon-chalkboard"></i> Asignaturas<i class="fa fa-caret-up"></i></a>
+                    <ul>
+                        <li><a href="asignatura-view.php"><i class="fa fa-eye"></i> Consulta</a></li>
+                        <li><a href="asignatura-edit.php"><i class="fa fa-pencil"></i> Modificación</a></li>
+                        <li><a href="asignatura-add.php"><i class="fa fa-plus-circle"></i> Alta</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="flaticon-diploma7"></i> Notas<i class="fa fa-caret-up"></i></a>
+                    <ul>
+                        <li><a href="nota-view.php"><i class="fa fa-eye"></i> Consulta</a></li>
+                        <li><a href="nota-edit.php"><i class="fa fa-pencil"></i> Modificación</a></li>
+                        <li><a href="nota-add.php"><i class="fa fa-plus-circle"></i> Alta</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="flaticon-boy31"></i> Usuarios<i class="fa fa-caret-up"></i></a>
+                    <ul>
+                        <li><a href="user-view.php"><i class="fa fa-eye"></i> Consulta</a></li>
+                        <li><a href="user-edit.php"><i class="fa fa-pencil"></i> Modificación</a></li>
+                        <li><a href="user-add.php"><i class="fa fa-plus-circle"></i> Alta</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="log.php"><i class="flaticon-gear26"></i> Log</a>
+                </li>
+                 <li>
+                    <a href="alumno-view.php"><i class="flaticon-gear26"></i> Ejemplo asignatura</a>
+                </li>
+            </ul>
+        </div>
